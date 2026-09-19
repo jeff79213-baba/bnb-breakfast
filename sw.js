0a9d1af46fd201ee010c58512555bfce2a00031a
馮小高 <jeff79213@gmail.com>
2026-09-19 13:00:50 +0800

    chore: sw 快取更版 v12 房號色點

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 民宿早餐房號管理系統 - Service Worker（離線快取） */
-const CACHE_NAME = 'bnb-breakfast-v11-sort-slots';
+const CACHE_NAME = 'bnb-breakfast-v12-order-dot';
 const APP_SHELL = [
   './',
   './index.html',
