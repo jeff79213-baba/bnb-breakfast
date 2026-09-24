ac4f03a6275d43b4877e1b118bd14d13f5edb69a
馮小高 <jeff79213@gmail.com>
2026-09-24 12:19:47 +0800

    chore: sw 快取更版 v18（姓名排序）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 民宿早餐房號管理系統 - Service Worker（離線快取） */
-const CACHE_NAME = 'bnb-breakfast-v17-addon-hot';
+const CACHE_NAME = 'bnb-breakfast-v18-sort-name';
 const APP_SHELL = [
   './',
   './index.html',
